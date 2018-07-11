e78254eceedf3632f7c151c767c37254bac50c99
Martin Canaan Mafunda <martdatanalytics@gmail.com>
2018-07-11 12:05:13 +0200

    new website look

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -3,13 +3,13 @@
 	$visitor_email = $_POST['email'];
 	$message = $_POST['message'];
 	
-	$email_from = 'martdatanlytics@gmail.com';
+	$email_from = 'martdatanalytics@gmail.com';
 	$email_subject = "Enquiry";
 	$email_body = "User name: $name.\n".
 					"User email: $email.\n".
 						"User message: $message.\n";
 						
-	$to = "martdatanlytics@gmail.com";
+	$to = "martdatanalytics@gmail.com";
 	$headers = "From: $email_from \r\n";
 	$headers = "Reply-to: $visitor_email \r\n";
 	mail($to, $email_from, $email_body, $headers);
